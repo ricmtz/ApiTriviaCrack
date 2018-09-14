2924bbee5ffbc92385bfcdf1c4db5ac6fbfb5c6f
Irving Cervantes Jiménez <irving.cervantes.inco@gmail.com>
2018-09-14 02:02:35 -0500

    #2 Post method implemented

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.post('/categories', (req, res) => {
+    const generatedId = 5;
+    const json = {
+        response: 'Created',
+        data: {
+            id: generatedId,
+            url: `https://domain/categories/${generatedId}`,
+        },
+    };
+    res.status(202).send(json);
+});
+
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
 });
