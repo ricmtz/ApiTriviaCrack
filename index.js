4dc0bd5480b278eb7146791e9a42fdcb2de63599
Irving Cervantes Jiménez <irving.cervantes.inco@gmail.com>
2018-09-14 02:29:31 -0500

    #2 Get category by id method implemented

--- index.js
+++ index.js
@@ -40,6 +40,19 @@ app.get('/categories', (req, res) => {
     res.status(200).send(json);
 });
 
+app.get('/categories/:categoryId', (req, res) => {
+    const json = {
+        response: 'Ok',
+        data: {
+            id: req.params.categoryId,
+            name: `Category ${req.params.categoryId}`,
+            color: 'White',
+            icon: `Icon${req.params.categoryId}.png`,
+        },
+    };
+    res.status(200).send(json);
+});
+
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
 });
