c59a373c2e0b46b9996ef1b3f8e675078f1a09ea
ricmtz <ricmtz15@gmail.com>
2018-09-14 23:36:55 -0500

    #12 Users endpoints crud created

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const usersRouter = require('./routes/users');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hola mundo');
 });
+
+app.use(usersRouter);
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
