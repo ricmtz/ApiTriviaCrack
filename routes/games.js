e24a299b6d24768b69a697f20d3b97756399f7d3
Irving Cervantes Jiménez <irving.cervantes.inco@gmail.com>
2018-09-17 18:09:58 -0500

    #15 Get all games method implemented

--- routes/games.js
+++ routes/games.js
@@ -15,4 +15,30 @@ router.post('/games', (req, res) => {
     res.status(201).send(json);
 });
 
+// Get all games
+router.get('/games', (req, res) => {
+    const games = [
+        {
+            id: 9,
+            player_1: 8,
+            player_2: 45,
+            answers_player_1: 3,
+            answers_player_2: 4,
+        },
+        {
+            id: 84,
+            player_1: 64,
+            player_2: 71,
+            answers_player_1: 6,
+            answers_player_2: 7,
+        },
+    ];
+    const json = {
+        response: 'Ok',
+        data: games,
+    };
+    res.status(200).send(json);
+});
+
+
 module.exports = router;
