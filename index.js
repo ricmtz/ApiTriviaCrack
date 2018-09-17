cd7afb111fc2e4e440740fb11ea78e1fd24c5a58
Irving Cervantes Jiménez <irving.cervantes.inco@gmail.com>
2018-09-17 14:56:51 -0500

    Fix #2 categories.js created in /routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const usersRouter = require('./routes/users');
+const categoriesRouter = require('./routes/categories');
 
 const app = express();
 
@@ -8,10 +9,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send('Hola mundo');
+    res.send('Hello World!');
 });
 
 app.use(usersRouter);
+app.use(categoriesRouter);
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
