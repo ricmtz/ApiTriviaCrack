4a401d3a77aa1c0f9bfc7907ace7a165362fb389
Adriana Castañeda <castalozano.adrigpe@gmail.com>
2018-09-17 16:38:15 -0500

    #14 update index.js for reading folder routers

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+const routers = require('./routers');
+
+app.use('/', routers);
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
