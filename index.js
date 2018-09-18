34b7dbba4ac9b6b5dcd423f8e410f5ec272b4a62
Irving Cervantes <42556654+IrvingCJ@users.noreply.github.com>
2018-09-18 10:23:25 -0500

    Revert "Dev"

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const usersRouter = require('./routes/users');
-const categoriesRouter = require('./routes/categories');
-const questionsUser = require('./routes/questions_users');
-const gamesQuestionsRouter = require('./routes/games_questions');
-const questionsRouter = require('./routes/questions');
-const games = require('./routes/games');
-const friendsRouter = require('./routes/friends');
 
 const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-
-app.use(usersRouter);
-app.use(categoriesRouter);
-app.use(questionsUser);
-app.use(gamesQuestionsRouter);
-app.use(questionsRouter);
-app.use(games);
-app.use(friendsRouter);
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
