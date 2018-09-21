e97f8d3f384131c51149d71d14ba25a724b6e223
ricmtz <ricmtz15@gmail.com>
2018-09-20 23:55:47 -0500

    #44 Singleton patter aplied to friends controller

--- controllers/friends.js
+++ controllers/friends.js
@@ -1,57 +1,69 @@
 
-exports.getAll = (req, res) => {
-    const fri = [
-        {
-            user_1: 1,
-            user_2: 3,
-            friendship_date: '01/15/2018',
-        },
-        {
-            user_1: 6,
-            user_2: 3,
-            friendship_date: '03/27/2018',
-        },
-    ];
-    const json = {
-        response: 'Ok',
-        data: fri,
-        total: 2,
-    };
-    res.status(200).send(json);
-};
+class Friends {
+    constructor() {
+        this.fri = [
+            {
+                user_1: 1,
+                user_2: 3,
+                friendship_date: '01/15/2018',
+            },
+            {
+                user_1: 6,
+                user_2: 3,
+                friendship_date: '03/27/2018',
+            },
+        ];
+        this.getAll = this.getAll.bind(this);
+        this.get = this.get.bind(this);
+        this.create = this.create.bind(this);
+        this.update = this.update.bind(this);
+        this.delete = this.delete.bind(this);
+    }
 
-exports.get = (req, res) => {
-    const json = {
-        response: 'Ok',
-        data: {
+    getAll(req, res) {
+        const json = {
+            response: 'Ok',
+            data: this.fri,
+            total: 2,
+        };
+        res.status(200).send(json);
+    }
+
+    get(req, res) {
+        const json = {
+            response: 'Ok',
+            data: {
+                user_1: req.params.user_1,
+                user_2: req.params.user_2,
+                friendship_date: '03/27/2018',
+            },
+        };
+        res.status(200).send(json);
+    }
+
+    create(req, res) {
+        const json = {
+            response: 'OK',
+            data: {
+                user_1: req.body.user_1,
+                user_2: req.body.user_2,
+            },
+        };
+        res.status(200).send(json);
+    }
+
+    update(req, res) {
+        res.status(204).send('Data is updated');
+    }
+
+    delete(req, res) {
+        const json = {
+            response: 'OK',
             user_1: req.params.user_1,
             user_2: req.params.user_2,
-            friendship_date: '03/27/2018',
-        },
-    };
-    res.status(200).send(json);
-};
+        };
+        res.status(200).send(json);
+    }
+}
 
-exports.create = (req, res) => {
-    const json = {
-        response: 'OK',
-        data: {
-            user_1: req.body.user_1,
-            user_2: req.body.user_2,
-        },
-    };
-    res.status(200).send(json);
-};
-
-exports.update = (req, res) => {
-    res.status(204).send('Data is updated');
-};
-
-exports.delete = (req, res) => {
-    const json = {
-        response: 'OK',
-        user_1: req.params.user_1,
-        user_2: req.params.user_2,
-    };
-    res.status(200).send(json);
-};
+module.exports = new Friends();
