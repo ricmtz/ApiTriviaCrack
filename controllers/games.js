335fbb41e68fd7d91a11de36c2808f230cf2905c
Irving Cervantes Jiménez <irving.cervantes.inco@gmail.com>
2018-09-21 20:09:24 -0500

    #45 Games controller restructured.

--- controllers/games.js
+++ controllers/games.js
@@ -1,76 +1,76 @@
+class GamesCtrl {
+    constructor() {
+        this.games = [
+            {
+                id: 9,
+                player_1: 8,
+                player_2: 45,
+                answers_player_1: 3,
+                answers_player_2: 4,
+            },
+            {
+                id: 84,
+                player_1: 64,
+                player_2: 71,
+                answers_player_1: 6,
+                answers_player_2: 7,
+            },
+        ];
+    }
 
-exports.getAll = (req, res) => {
-    const games = [
-        {
-            id: 9,
-            player_1: 8,
-            player_2: 45,
-            answers_player_1: 3,
-            answers_player_2: 4,
-        },
-        {
-            id: 84,
-            player_1: 64,
-            player_2: 71,
-            answers_player_1: 6,
-            answers_player_2: 7,
-        },
-    ];
-    const json = {
-        response: 'Ok',
-        data: games,
-    };
-    res.status(200).send(json);
-};
+    getAll(req, res) {
+        const json = {
+            response: 'Ok',
+            data: this.games,
+        };
+        res.status(200).send(json);
+    }
 
-exports.get = (req, res) => {
-    const json = {
-        response: 'Ok',
-        data: {
-            id: `${req.params.gameId}`,
-            player_1: 48,
-            player_2: 24,
-            answers_player_1: 0,
-            answers_player_2: 2,
-        },
-    };
-    res.status(200).send(json);
-};
+    get(req, res) {
+        const json = {
+            response: 'Ok',
+            data: this.gamesQuestions.find(el => el.id === Number(req.params.gameId)),
+        };
+        res.status(200).send(json);
+    }
 
-exports.create = (req, res) => {
-    const generatedId = 15;
-    const json = {
-        response: 'Created',
-        data: {
-            id: generatedId,
-            url: `https://domain/games/${req.body.player_1}&${req.body.player_2}`,
-        },
-    };
-    res.status(201).send(json);
-};
+    create(req, res) {
+        const generatedId = 15;
+        const json = {
+            response: 'Created',
+            data: {
+                id: generatedId,
+                url: `https://domain/games/${req.body.player_1}&${req.body.player_2}`,
+            },
+        };
+        res.status(201).send(json);
+    }
 
-exports.update = (req, res) => {
-    const json = {
-        response: 'Ok',
-        data: {
-            id: req.params.gameId,
-        },
-    };
-    res.status(200).send(json);
-};
+    update(req, res) {
+        const json = {
+            response: 'Ok',
+            data: {
+                id: req.params.gameId,
+            },
+        };
+        res.status(200).send(json);
+    }
 
-exports.delete = (req, res) => {
-    const json = {
-        response: 'No content',
-        data: {},
-    };
-    res.status(204).send(json);
-};
+    delete(req, res) {
+        const json = {
+            response: 'No content',
+            data: {},
+        };
+        res.status(204).send(json);
+    }
 
-exports.deleteAll = (req, res) => {
-    const json = {
-        response: 'No content',
-        data: {},
-    };
-    res.status(204).send(json);
-};
+    deleteAll(req, res) {
+        const json = {
+            response: 'No content',
+            data: {},
+        };
+        res.status(204).send(json);
+    }
+}
+
+module.exports = new GamesCtrl();
