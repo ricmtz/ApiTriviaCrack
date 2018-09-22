888464bca3c210bd1279cb67284b0af76065cd08
Irving Cervantes Jiménez <irving.cervantes.inco@gmail.com>
2018-09-21 19:51:21 -0500

    #45 Users controller restructured.

--- controllers/users.js
+++ controllers/users.js
@@ -1,59 +1,60 @@
+class UsersCtrl {
+    constructor() {
+        this.users = [
+            {
+                id: 1,
+                nickname: 'xXPedro777Xx',
+                email: 'jose@gmail.com',
+                current_points: 777,
+            },
+            {
+                id: 2,
+                nickname: 'xXRuben777Xx',
+                email: 'ruben@gmail.com',
+                current_points: 767,
+            },
+        ];
+    }
 
-exports.getAll = (req, res) => {
-    const users = [
-        {
-            id: 1,
-            nickname: 'xXPedro777Xx',
-            email: 'jose@gmail.com',
-            current_points: 777,
-        },
-        {
-            id: 2,
-            nickname: 'xXRuben777Xx',
-            email: 'ruben@gmail.com',
-            current_points: 767,
-        },
-    ];
-    const json = {
-        response: 'Ok',
-        data: users,
-        total: 2,
-    };
-    res.status(200).send(json);
-};
+    getAll(req, res) {
+        const json = {
+            response: 'Ok',
+            data: this.users,
+            total: 2,
+        };
+        res.status(200).send(json);
+    }
 
-exports.get = (req, res) => {
-    const json = {
-        response: 'Ok',
-        data: {
+    get(req, res) {
+        const json = {
+            response: 'Ok',
+            data: this.users.find(el => el.id === Number(req.params.userId)),
+        };
+        res.status(200).send(json);
+    }
+
+    create(req, res) {
+        const json = {
+            response: 'Ok',
+            data: {
+                id: 100,
+                nickname: req.body.nickname,
+            },
+        };
+        res.status(200).send(json);
+    }
+
+    update(req, res) {
+        res.status(204).send('Data successfully updated');
+    }
+
+    delete(req, res) {
+        const json = {
+            response: 'Ok',
             id: req.params.userId,
-            nickname: 'xXRuben777Xx',
-            email: 'ruben@gmail.com',
-            current_points: 767,
-        },
-    };
-    res.status(200).send(json);
-};
+        };
+        res.status(200).send(json);
+    }
+}
 
-exports.create = (req, res) => {
-    const json = {
-        response: 'Ok',
-        data: {
-            id: 100,
-            nickname: req.body.nickname,
-        },
-    };
-    res.status(200).send(json);
-};
-
-exports.update = (req, res) => {
-    res.status(204).send('Data successfully updated');
-};
-
-exports.delete = (req, res) => {
-    const json = {
-        response: 'Ok',
-        id: req.params.userId,
-    };
-    res.status(200).send(json);
-};
+module.exports = new UsersCtrl();
