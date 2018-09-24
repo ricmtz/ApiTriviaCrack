7fe28efc6dac1c41e63feccf86ecf00df24a19da
ricmtz <ricmtz15@gmail.com>
2018-09-24 16:21:19 -0500

    #50 Creation of user model

---
+++ models/users.js
@@ -0,0 +1,38 @@
+const db = require('../db');
+
+class User {
+    /**
+     * @param {Number} id
+     * @param {String} nickname
+     * @param {String} password
+     * @param {String} email
+     * @param {String} uType
+     * @param {number} cPoints
+     */
+    constructor(id, nickname, password, email, uType, cPoints) {
+        this.id = id;
+        this.nickname = nickname;
+        this.password = password;
+        this.email = email;
+        this.userType = uType;
+        this.currentPoints = cPoints;
+    }
+
+    save() {
+        db.create(this);
+    }
+
+    getNickname() {
+        return this.nickname;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getCurrentPoints() {
+        return this.currentPoints;
+    }
+}
+
+module.exports = User;
