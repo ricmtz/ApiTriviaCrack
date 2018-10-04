c1ed58b5e65b141e7f0ffb78ec787747666f9eed
Adriana Castañeda <castalozano.adrigpe@gmail.com>
2018-10-03 22:22:49 -0500

    #58 update insert and select methods of db

--- db/db.js
+++ db/db.js
@@ -28,18 +28,19 @@ class DB {
             });
     }
 
-    getValuesAssing(data) {
+    getValuesAssing(data, extra) {
         return new Promise((resolve) => {
             const columnsArr = Object.keys(data);
             const valuesArr = Object.values(data);
             this.values = '';
             for (let i = 0; i < valuesArr.length; i += 1) {
-                if (i !== 0) this.values += ', ';
+                if (i !== 0) this.values += extra;
                 let valueAdd = '';
                 if (isNaN(valuesArr[i])) valueAdd = `'${valuesArr[i]}'`;
                 else valueAdd = `${valuesArr[i]}`;
                 this.values += ` ${columnsArr[i]} = ${valueAdd}`;
             }
+            console.log(this.values);
             resolve(this.values);
         });
     }
@@ -57,14 +58,18 @@ class DB {
         });
     }
 
-    async select(table, columns = [], conditions = null) {
+    async select(table, columns = [], conditions = null, distinct = false) {
+        if (table === undefined) return 'Error: table name is undefined';
         let col = '*';
+        let dis = '';
+        let cond = '';
         if (columns.length !== 0) col = columns.toString();
-        let query = `SELECT ${col} FROM ${table}`;
+        if (distinct) dis = 'DISTINCT';
         if (conditions !== null) {
-            const where = await this.getValuesAssing(conditions);
-            query += ` WHERE ${where}`;
+            const where = await this.getValuesAssing(conditions, ' AND ');
+            cond = `WHERE ${where}`;
         }
+        const query = `SELECT ${dis} ${col} FROM ${table} ${cond} `;
         this.values = '';
         return new Promise((resolve, reject) => {
             this.db.query(query)
