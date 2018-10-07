312094ef3f1d2c9e3259764178bfddcb6f7dff9b
Irving Cervantes Jiménez <irving.cervantes.inco@gmail.com>
2018-10-07 02:59:30 -0500

    #65 GamesQuestions router updated. Endpoints unchanged.

--- routes/games_questions.js
+++ routes/games_questions.js
@@ -1,21 +1,21 @@
 const { Router } = require('express');
 const { gamesCtrl } = require('../controllers');
 
-const router = Router();
+const router = Router({ mergeParams: true });
 
 // Get all games_questions
-router.get('/:gameId/games_questions', gamesCtrl.getAllGamesQuestions);
+router.get('/', gamesCtrl.getAllGamesQuestions);
 
 // Get question of game
-router.get('/:gameId/:questionId', gamesCtrl.getGameQuestion);
+router.get('/:questionId', gamesCtrl.getGameQuestion);
 
 // Create questions
-router.post('/:gameId/games_questions', gamesCtrl.createQuestions);
+router.post('/', gamesCtrl.createQuestions);
 
 // Delete question
-router.delete('/:gameId/:questionId', gamesCtrl.removeQuestion);
+router.delete('/:questionId', gamesCtrl.removeQuestion);
 
 // Update question
-router.patch('/:gameId/:questionId', gamesCtrl.updateQuestion);
+router.patch('/:questionId', gamesCtrl.updateQuestion);
 
 module.exports = router;
