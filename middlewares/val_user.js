50decaeeb91643d7fd4e93396764d479468a218f
ricmtz <ricmtz15@gmail.com>
2018-10-08 18:58:25 -0500

    #72 update validation added

--- middlewares/val_user.js
+++ middlewares/val_user.js
@@ -18,6 +18,19 @@ class ValUsers {
             },
         });
     }
+
+    static update(req, res, next) {
+        validator.validate(req, res, next, {
+            body: {
+                nickname: 'nickname',
+                password: 'password',
+                email: 'email',
+                score: 'positive',
+                admin: 'boolean',
+                avatar: 'file',
+            },
+        });
+    }
 }
 
 module.exports = ValUsers;
