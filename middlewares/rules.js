c128f756c54ef457445d6c8d0c8afa5d5cd9a4f3
ricmtz <ricmtz15@gmail.com>
2018-10-09 00:36:51 -0500

    #72 middleware with all the rules to validate the endpoints

---
+++ middlewares/rules.js
@@ -0,0 +1,86 @@
+const validator = require('./validator');
+
+class Rules {
+    static createUser(req, res, next) {
+        validator.validate(req, res, next, {
+            body: {
+                nickname: 'nickname,required',
+                password: 'password,required',
+                email: 'email,required',
+            },
+        });
+    }
+
+    static paramsUser(req, res, next) {
+        validator.validate(req, res, next, {
+            params: {
+                nickname: 'nickname,required',
+            },
+        });
+    }
+
+    static updateUser(req, res, next) {
+        validator.validate(req, res, next, {
+            params: {
+                nickname: 'nickname',
+            },
+            body: {
+                nickname: 'nickname,optional',
+                password: 'password,optional',
+                email: 'email,optional',
+                score: 'positive,optional',
+                admin: 'boolean,optional',
+                avatar: 'file,optional',
+            },
+        });
+    }
+
+    static createFriend(req, res, next) {
+        validator.validate(req, res, next, {
+            params: {
+                nickname: 'nickname',
+            },
+            body: {
+                nickname: 'nickname,required',
+            },
+        });
+    }
+
+    static paramsFriends(req, res, next) {
+        validator.validate(req, res, next, {
+            params: {
+                nickname: 'nickname',
+                friendNickname: 'nickname',
+            },
+        });
+    }
+
+    static addEmail(req, res, next) {
+        validator.validate(req, res, next, {
+            body: {
+                email: 'email,required',
+            },
+        });
+    }
+
+    static updateEmail(req, res, next) {
+        validator.validate(req, res, next, {
+            params: {
+                emailId: 'id,required',
+            },
+            body: {
+                email: 'email,required',
+            },
+        });
+    }
+
+    static paramsEmails(req, res, next) {
+        validator.validate(req, res, next, {
+            params: {
+                emailId: 'id,required',
+            },
+        });
+    }
+}
+
+module.exports = Rules;
