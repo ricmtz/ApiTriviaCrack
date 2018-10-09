a04476731afae835186446521feab6c10551c9e6
Adriana Castañeda <castalozano.adrigpe@gmail.com>
2018-10-08 19:20:12 -0500

    #58 update select, add join

--- db/db.js
+++ db/db.js
@@ -57,18 +57,43 @@ class DB {
         });
     }
 
-    async select(table, columns = [], conditions = null, distinct = false) {
+    getJoins(joins) {
+        return new Promise((resolve) => {
+            this.result = '';
+            joins.forEach((join) => {
+                this.result += ` ${join.type} ${join.table} AS ${join.as} ON ${join.condition}`;
+            });
+            resolve(this.result);
+        });
+    }
+
+    getColumns(columns) {
+        return new Promise((resolve) => {
+            this.result = '';
+            columns.forEach((column, i) => {
+                if (column.column === undefined) resolve(columns.toString());
+                if (i !== 0) this.result += ',';
+                this.result += ` ${column.column} `;
+                if (column.as !== undefined) this.result += `AS ${column.as}`;
+            });
+            resolve(this.result);
+        });
+    }
+
+    async select(table, columns = [], conditions = null, extra = ' AND ', join = [], distinct = false) {
         if (table === undefined) return 'Error: table name is undefined';
         let col = '*';
         let dis = '';
         let cond = '';
-        if (columns.length !== 0) col = columns.toString();
+        let joins = '';
+        if (columns.length !== 0) col = await this.getColumns(columns);
         if (distinct) dis = 'DISTINCT';
         if (conditions !== null) {
-            const where = await this.getValuesAssing(conditions, ' AND ');
+            const where = await this.getValuesAssing(conditions, extra);
             cond = `WHERE ${where}`;
         }
-        const query = `SELECT ${dis} ${col} FROM ${table} ${cond} `;
+        if (join !== []) joins = await this.getJoins(join);
+        const query = `SELECT ${dis} ${col} FROM ${table} ${joins} ${cond} `;
         this.values = '';
         return new Promise((resolve, reject) => {
             this.db.query(query)
@@ -85,15 +110,15 @@ class DB {
         return new Promise((resolve, reject) => {
             this.db.query(query)
                 .then(res => resolve(res))
-                .catch(e => reject(e.stack));
+                .catch(e => reject(e));
         });
     }
 
-    async update(table, data, conditions) {
+    async update(table, data, conditions, extra = ' AND ') {
         const values = await this.getValuesAssing(data, ', ');
         let cond = '';
         if (conditions !== null) {
-            const where = await this.getValuesAssing(conditions, ' AND ');
+            const where = await this.getValuesAssing(conditions, extra);
             cond = `WHERE ${where}`;
         }
         const query = `UPDATE ${table} SET ${values} ${cond}`;
@@ -101,7 +126,22 @@ class DB {
         return new Promise((resolve, reject) => {
             this.db.query(query)
                 .then(res => resolve(res))
-                .catch(e => reject(e.stack));
+                .catch(e => reject(e));
+        });
+    }
+
+    async delete(table, conditions, extra = ' AND ') {
+        let cond = '';
+        if (conditions !== null) {
+            const where = await this.getValuesAssing(conditions, extra);
+            cond = `WHERE ${where}`;
+        }
+        const query = `DELETE FROM ${table} ${cond}`;
+        this.values = '';
+        return new Promise((resolve, reject) => {
+            this.db.query(query)
+                .then(res => resolve(res))
+                .catch(e => reject(e));
         });
     }
 }
