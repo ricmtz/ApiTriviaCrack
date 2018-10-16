a0a1d5b77064d3b94964113b90c29fc013727139
Irving Cervantes Jiménez <irving.cervantes.inco@gmail.com>
2018-10-16 05:54:51 -0500

    #81 Tokens ORM created.

---
+++ orm/tokens.js
@@ -0,0 +1,63 @@
+const { db } = require('../db');
+const { Token } = require('../models');
+
+class Tokens {
+    constructor() {
+        this.name = 'tokens';
+        this.msgNoToken = 'Token not found';
+        this.msgExistToken = 'Token already exists';
+        this.msgNoCreateToken = 'Could not create token';
+    }
+
+    async active(searchToken) {
+        const exist = await this.existData(this.name, { token: searchToken });
+        if (exist) return this.msgExistToken;
+
+        const tokenObj = await db.select(this.name, [], { token: searchToken });
+        if (tokenObj.getExpires() - new Date() < 0) {
+            await db.update(this.name, { status: '0' }, { token: tokenObj.getToken() });
+            return false;
+        }
+        return true;
+    }
+
+    async get(searchToken) {
+        const data = { token: searchToken };
+        const result = await db.select(this.name, [], data);
+        return result.length !== 0 ? new Token(result[0]) : this.msgNoToken;
+    }
+
+    async existData(table, condition) {
+        const result = await db.select(table, ['count(*)'], condition);
+        return (result[0].count != 0);
+    }
+
+    async create(data) {
+        const tokenObj = new Token(data);
+
+        const exist = await this.existData(this.name, { token: tokenObj.getName() });
+        if (exist) return this.msgExistToken;
+
+        let result = await db.insert(this.name, tokenObj);
+        result = await db.select(this.name, ['id'], tokenObj);
+
+        if (result.length !== 0) {
+            tokenObj.setId(result[0].id);
+            return tokenObj;
+        }
+        return this.msgNoCreateToken;
+    }
+
+    async delete(searchToken) {
+        let res = await db.select(this.name, ['id'], { token: searchToken });
+        if (res.length === 0) return this.msgNoToken;
+
+        const data = { status: '0' };
+        res = await db.update(this.name, data, { token: searchToken });
+
+        res = await db.select(this.name, ['id'], data);
+        return res;
+    }
+}
+
+module.exports = Tokens;
