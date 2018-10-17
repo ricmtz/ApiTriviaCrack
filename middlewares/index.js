1b48b21c09cd4b9d9098a1306c20084e6792c5fe
ricmtz <ricmtz15@gmail.com>
2018-10-17 17:08:17 -0500

    #87 deafault_values.js dependencies updated to defaultValues.js

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,4 +2,4 @@ exports.validator = require('./validator');
 exports.errorHandler = require('./errorHandler');
 exports.generator = require('./generator');
 exports.rules = require('./rules');
-exports.defaultValues = require('./default_values');
+exports.defaultValues = require('./defaultValues');
