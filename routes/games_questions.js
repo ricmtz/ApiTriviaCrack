2ace380c73a2085026e81f612b5ee2208e7086b3
ricmtz <ricmtz15@gmail.com>
2018-10-17 23:28:13 -0500

    #87 questionId parameter validator added

--- routes/games_questions.js
+++ routes/games_questions.js
@@ -4,22 +4,22 @@ const { rules } = require('../middlewares');
 
 const router = Router({ mergeParams: true });
 
+// Validation param questionId
+router.use('/:questionId', rules.paramsGamesQuestions);
+
 // Get all games_questions
 router.get('/', gamesQuestionsCtrl.getAll);
 
 // Get question of game
-// FIXME Falta validar el param :questionId
 router.get('/:questionId', gamesQuestionsCtrl.get);
 
 // Create questions
 router.post('/', rules.createGameQuestion, gamesQuestionsCtrl.create);
 
 // Delete question
-// FIXME Falta validar el param :questionId
 router.delete('/:questionId', gamesQuestionsCtrl.delete);
 
 // Update question
-// FIXME Falta validar el param :questionId
 router.patch('/:questionId', rules.updateGameQuestion, gamesQuestionsCtrl.update);
 
 module.exports = router;
