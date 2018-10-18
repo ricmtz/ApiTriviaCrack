04f20351dfb0fc0335fd508b0cb143b389d6c8ed
ricmtz <ricmtz15@gmail.com>
2018-10-17 23:25:14 -0500

    #87 categoryId parameter validator added

--- routes/categories.js
+++ routes/categories.js
@@ -4,6 +4,9 @@ const { rules } = require('../middlewares');
 
 const router = Router();
 
+// Validation param categoryId
+router.use('/:categoryId', rules.paramsCategories);
+
 // Create category
 router.post('/', rules.createCategory, categoriesCtrl.create);
 
@@ -11,15 +14,12 @@ router.post('/', rules.createCategory, categoriesCtrl.create);
 router.get('/', categoriesCtrl.getAll);
 
 // Get category
-// FIXME Se tiene que validar el parametro :categoryId que sea un identificador valido, ejem: un numero en cierto rango
 router.get('/:categoryId', categoriesCtrl.get);
 
 // Put category
-// FIXME Se tiene que validar el parametro :categoryId que sea un identificador valido, ejem: un numero en cierto rango
 router.patch('/:categoryId', rules.updateCategory, categoriesCtrl.update);
 
 // Delete category
-// FIXME Se tiene que validar el parametro :categoryId que sea un identificador valido, ejem: un numero en cierto rango
 router.delete('/:categoryId', categoriesCtrl.delete);
 
 module.exports = router;
