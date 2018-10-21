e3bb12905398c3d8801e995575c149574aac5d47
Irving Cervantes Jiménez <irving.cervantes.inco@gmail.com>
2018-10-20 22:38:13 -0500

    #90 Db restructured, methods reordered.

--- db/db.js
+++ db/db.js
@@ -3,6 +3,8 @@ const pgp = require('pg-promise')({
 });
 require('dotenv').config();
 
+const CHECK_DELETED_ENTRIES = true;
+const DEFAULT_LOG_OP = ' AND ';
 
 const cn = {
     host: process.env.DB_HOST,
@@ -18,19 +20,31 @@ class DB {
         this.db = pgp(cn);
     }
 
-    async exists(table, conditions, deleted = false) {
-        return new Promise((resolve, reject) => {
-            this.db.many(pgp.as.format('SELECT * FROM $<tab#> $<where^>',
-                {
-                    tab: table,
-                    where: DB.getWhere(conditions, ' AND ', deleted),
-                }))
-                .then(res => resolve(res))
-                .catch(err => reject(err));
-        });
+    selectQuery(table, conditions = null, del = false, columns = [],
+        limit = 10, offset = 0, logOp = DEFAULT_LOG_OP) {
+        return pgp.as.format('SELECT $<cols^> FROM $<tab#> $<where^> LIMIT $<lim#> OFFSET $<off#>',
+            {
+                cols: this.getColsQuery(columns),
+                tab: table,
+                where: this.getWhere(conditions, logOp, del),
+                lim: limit,
+                off: offset,
+            });
     }
 
-    static getWhere(conditions, logOp, deleted = false) {
+    insertQuery(table, data) {
+        return pgp.helpers.insert(data, null, table);
+    }
+
+    updateQuery(table, data, conditions, logOp = DEFAULT_LOG_OP) {
+        return `${pgp.helpers.update(data, null, table)} ${this.getWhere(conditions, logOp)}`;
+    }
+
+    deleteQuery(table, conditions) {
+        return this.updateQuery(table, { deleted: true }, conditions);
+    }
+
+    getWhere(conditions, logOp, deleted = false) {
         if (!conditions) {
             return '';
         }
@@ -42,57 +56,53 @@ class DB {
         return `WHERE ${pgp.helpers.sets(conds).replace(',', logOp)}`;
     }
 
-    static getColsQuery(cols) {
+    getColsQuery(cols) {
         return (cols && cols.length) ? pgp.helpers.ColumnSet(cols).names : '*';
     }
 
-    async select(table, columns = [], conditions = null, limit = 10, offset = 0, extra = ' AND ') {
-        if (!table === undefined) {
-            return Promise.reject(new Error('Table name is undefined'));
-        }
-
-        const query = pgp.as.format('SELECT $<cols^> FROM $<tab#> $<where^> LIMIT $<lim#> OFFSET $<off#>',
-            {
-                cols: DB.getColsQuery(columns),
-                tab: table,
-                where: DB.getWhere(conditions, extra),
-                lim: limit,
-                off: offset,
-            });
-
+    async exists(table, conditions, deleted = CHECK_DELETED_ENTRIES) {
         return new Promise((resolve, reject) => {
-            this.db.many(query)
+            if (!table) {
+                reject(new Error('Table name is undefined'));
+            }
+
+            this.db.one(this.selectQuery(table, conditions, deleted))
+                .then(res => resolve(res))
+                .catch(err => reject(err));
+        });
+    }
+
+    async select(table, conditions = null, columns = []) {
+        return new Promise((resolve, reject) => {
+            this.db.many(this.selectQuery(table, conditions, false, columns))
                 .then(res => resolve(res))
                 .catch(err => reject(err));
         });
     }
 
     async insert(table, data) {
-        const query = pgp.helpers.insert(data, null, table);
         return new Promise((resolve, reject) => {
-            this.db.none(query)
+            this.db.none(this.insertQuery(table, data))
                 .then(res => resolve(res))
                 .catch(e => reject(e));
         });
     }
 
-    async update(table, data, conditions, extra = ' AND ') {
-        const query = `${pgp.helpers.update(data, null, table)} ${DB.getWhere(conditions, extra)}`;
+    async update(table, data, conditions) {
         return new Promise((resolve, reject) => {
-            this.db.none(query)
+            this.db.none(this.updateQuery(table, data, conditions))
                 .then(res => resolve(res))
                 .catch(e => reject(e));
         });
     }
 
-    async delete(table, conditions, extra = ' AND ') {
+    async delete(table, conditions) {
         if (!conditions) {
             return Promise.reject(new Error(`Deletion of all ${table} is not supported`));
         }
 
-        const query = `${pgp.helpers.update({ deleted: true }, null, table)} ${DB.getWhere(conditions, extra)}`;
         return new Promise((resolve, reject) => {
-            this.db.none(query)
+            this.db.none(this.deleteQuery(table, conditions))
                 .then(res => resolve(res))
                 .catch(err => reject(err));
         });
