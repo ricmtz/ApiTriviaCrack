817813da024a10fe4da49a488f3ce1206c879fed
Irving Cervantes Jiménez <irving.cervantes.inco@gmail.com>
2018-10-20 22:40:52 -0500

    #90 Error message is now correctly sent.

--- controllers/users.js
+++ controllers/users.js
@@ -15,25 +15,25 @@ class UsersCtrl {
     async get(req, res) {
         await UsersORM.getByNickname(req.params.nickname)
             .then((usr) => { res.status(200).send({ data: usr }); })
-            .catch((err) => { res.status(404).send({ data: err }); });
+            .catch((err) => { res.status(404).send({ data: err.message }); });
     }
 
     async create(req, res) {
         await UsersORM.create(req.body)
             .then((usr) => { res.status(200).send({ data: usr }); })
-            .catch((err) => { res.status(404).send({ data: err }); });
+            .catch((err) => { res.status(404).send({ data: err.message }); });
     }
 
     async update(req, res) {
         await UsersORM.update(req.params.nickname, req.body)
             .then(() => { res.status(204).send(); })
-            .catch((err) => { res.status(404).send({ data: err }); });
+            .catch((err) => { res.status(404).send({ data: err.message }); });
     }
 
     async delete(req, res) {
         await UsersORM.delete(req.params.nickname)
             .then(() => { res.status(204).send(); })
-            .catch((err) => { res.status(404).send({ data: err }); });
+            .catch((err) => { res.status(404).send({ data: err.message }); });
     }
 }
 
