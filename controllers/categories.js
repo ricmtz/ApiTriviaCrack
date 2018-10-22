f05088e65e16f9ceaa4773aafd3a937c88b09d0d
ricmtz <ricmtz15@gmail.com>
2018-10-22 01:43:08 -0500

    #90 Categories controller update

--- controllers/categories.js
+++ controllers/categories.js
@@ -2,53 +2,38 @@ const { CategoriesORM } = require('../orm');
 
 class CategoriesCtrl {
     async getAll(req, res) {
-      const result = await CategoriesORM.getAll();
-      const json = {
-          data: result,
-          total: result.length,
-      };
-      if (result.length === 0) res.status(404);
-      res.send(json);
+        await CategoriesORM.getAll(req.query)
+            .then((categ) => {
+                res.status(200).send({
+                    data: categ,
+                    total: categ.length,
+                });
+            })
+            .catch((err) => { res.status(404).send({ data: err.message }); });
     }
 
     async get(req, res) {
-        const result = await CategoriesORM.getCategory(req.params.category);
-        const json = {
-            data: result,
-        };
-        if (result.length === 0) res.status(404);
-        res.send(json);
+        await CategoriesORM.get(req.params)
+            .then((categ) => { res.status(200).send({ data: categ, }); })
+            .catch((err) => { res.status(404).send({ data: err.message, }); });
     }
 
     async create(req, res) {
-        const result = await CategoriesORM.create(req.body);
-        const json = {
-            data: result,
-        };
-        if (result.length === 0) res.status(404);
-        res.status(200).send(json);
+        await CategoriesORM.create(req.body)
+            .then((categ) => { res.status(200).send({ data: categ, }); })
+            .catch((err) => { res.status(404).send({ data: err.message, }); });
     }
 
     async update(req, res) {
-        const result = await CategoriesORM.update(req.params.category, req.body);
-        const json = {
-            data: result,
-        };
-        if (result.length === 0) {
-            res.status(404);
-        } else {
-            res.status(204);
-        }
-        res.send(json);
+        await CategoriesORM.update(req.params, req.body)
+            .then((categ) => { res.status(200).send({ data: categ, }); })
+            .catch((err) => { res.status(404).send({ data: err, message, }); });
     }
 
     async delete(req, res) {
-        const result = await CategoriesORM.delete(req.params.category);
-        const json = {
-            data: result,
-        };
-        if (result.length === 0) res.status(404);
-        res.send(json);
+        await CategoriesORM.delete(req.params)
+            .then(() => { res.status(204).send(); })
+            .catch((err) => { res.status(404).send({ data: err.message }); });
     }
 }
 
