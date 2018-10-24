9d670a00cb7bd4fe1e9e10c15a4c030d64e6ee31
ricmtz <ricmtz15@gmail.com>
2018-10-24 15:55:48 -0500

    #90 correction of games_questions controller

--- controllers/games_questions.js
+++ controllers/games_questions.js
@@ -5,7 +5,7 @@ const { GamesORM } = require('../orm');
 class GamesQuestionsCtrl {
     // FIXME En los metodos getAll se debe permitir paginado y filtrado
     async getAll(req, res) {
-        await GamesORM.getAllGamesQuestions(req.params.gameId)
+        await GamesORM.getAllGamesQuestions(req.params.gameId, req.query.page)
             .then((ans) => {
                 res.status(200).send({
                     data: ans,
@@ -24,42 +24,33 @@ class GamesQuestionsCtrl {
     async create(req, res) {
         const data = {
             game: req.params.gameId,
-            question: req.body.question,
+            question: req.params.questionId,
             player: req.body.player,
             option: req.body.option,
+            correct: req.body.correct,
         };
-        const result = await GamesORM.addAnswer(data);
-        const finish = await GamesORM.finishGame(data);
-        const json = {
-            data: result,
-            finish_game: finish,
-        };
-        if (result.length === 0) res.status(404);
-        res.status(200).send(json);
+        await GamesORM.addAnswer(data)
+            .then((ans) => { res.status(200).send({ data: ans }); })
+            .catch((err) => { res.status(404).send({ data: err.message }); });
     }
 
     async update(req, res) {
         const data = {
-            id: req.params.questionId,
             game: req.params.gameId,
-            question: req.body.question,
+            question: req.params.questionId,
             player: req.body.player,
-            selectedoption: req.body.selectedoption,
+            option: req.body.selectedoption,
         };
-        const result = await GamesORM.updateGameQuestion(data);
-        const json = {
-            response: 'Ok',
-            data: result,
-        };
-        res.status(200).send(json);
+        console.log(data);
+        await GamesORM.updateGameQuestion(req.params.gameId, req.params.questionId, data)
+            .then((ans) => { res.status(204).send() })
+            .catch((err) => { res.status(404).send({ data: err.message }); });
     }
 
     async delete(req, res) {
-        const json = {
-            response: 'Ok',
-            data: `Question ${req.params.questionId} removed from ${req.params.gameId}`,
-        };
-        res.status(200).send(json);
+        await GamesORM.deleteGameQuestion(req.params.gameId, req.params.questionId)
+            .then(() => { res.status(204).send(); })
+            .catch((err) => { res.status(404).send({ data: err.message }) });
     }
 }
 
