5868e7513328a276e3d452bf8c9574800154ed34
ricmtz <ricmtz15@gmail.com>
2018-10-29 14:53:14 -0600

    #90 minor adjustemts in script to create the data base

--- script_db.sql
+++ script_db.sql
@@ -2,31 +2,31 @@
 CREATE TABLE users
 (
     id SERIAL,
-    nickname TEXT COLLATE pg_catalog."default" NOT NULL,
-    password TEXT COLLATE pg_catalog."default" NOT NULL,
-    email TEXT COLLATE pg_catalog."default" NOT NULL,
+    nickname TEXT NOT NULL,
+    password TEXT NOT NULL,
+    email TEXT NOT NULL,
     score INTEGER DEFAULT 0,
-    avatar TEXT COLLATE pg_catalog."default" DEFAULT ''::TEXT,
+    avatar TEXT DEFAULT ''::TEXT,
     lastlogin TIMESTAMP,
     deleted BOOLEAN DEFAULT false,
     admin BOOLEAN DEFAULT false,
     CONSTRAINT users_pkey PRIMARY KEY (id),
     CONSTRAINT users_nickname_key UNIQUE (nickname),
     CONSTRAINT users_principal_email_key UNIQUE (email)
-)
+);
 
 CREATE TABLE emails
 (
     id SERIAL,
     userid INTEGER,
-    email TEXT COLLATE pg_catalog."default",
+    email TEXT,
     deleted BOOLEAN DEFAULT false,
     CONSTRAINT emails_email_key UNIQUE (email),
     CONSTRAINT emails_id_user_fkey FOREIGN KEY (userid)
         REFERENCES users (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
-)
+);
 
 CREATE TABLE friends
 (
@@ -35,7 +35,7 @@ CREATE TABLE friends
     user2 INTEGER NOT NULL,
     friendshipdate TIMESTAMP NOT NULL,
     deleted BOOLEAN DEFAULT false,
-    CONSTRAINT friends_pkey PRIMARY KEY (user1, user2),
+    CONSTRAINT friends_pkey PRIMARY KEY (id),
     CONSTRAINT friends_id_user_1_fkey FOREIGN KEY (user1)
         REFERENCES users (id) MATCH SIMPLE
         ON UPDATE NO ACTION
@@ -44,7 +44,7 @@ CREATE TABLE friends
         REFERENCES users (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
-)
+);
 
 CREATE TABLE games
 (
@@ -65,30 +65,30 @@ CREATE TABLE games
         REFERENCES users (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
-)
+);
 
 CREATE TABLE categories
 (
     id SERIAL,
-    name TEXT COLLATE pg_catalog."default" NOT NULL,
-    color TEXT COLLATE pg_catalog."default" NOT NULL,
-    icon TEXT COLLATE pg_catalog."default" DEFAULT ''::TEXT,
+    name TEXT NOT NULL,
+    color TEXT NOT NULL,
+    icon TEXT DEFAULT ''::TEXT,
     deleted BOOLEAN DEFAULT false,
     CONSTRAINT categories_pkey PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE questions
 (
     id SERIAL,
     category INTEGER,
-    question TEXT COLLATE pg_catalog."default" NOT NULL,
-    option1 TEXT COLLATE pg_catalog."default" NOT NULL,
-    option2 TEXT COLLATE pg_catalog."default" NOT NULL,
-    optioncorrect TEXT COLLATE pg_catalog."default" NOT NULL,
+    question TEXT NOT NULL,
+    option1 TEXT NOT NULL,
+    option2 TEXT NOT NULL,
+    optioncorrect TEXT NOT NULL,
     approved BOOLEAN DEFAULT false,
     deleted BOOLEAN DEFAULT false,
     userid INTEGER,
-    createdate TIMESTAMP,
+    createdate TIMESTAMP NOT NULL,
     approveddate TIMESTAMP,
     CONSTRAINT questions_pkey PRIMARY KEY (id),
     CONSTRAINT questions_category_fkey FOREIGN KEY (category)
@@ -99,44 +99,44 @@ CREATE TABLE questions
         REFERENCES users (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
-)
+);
 
-CREATE TABLE games_questions
+CREATE TABLE answers
 (
     id SERIAL,
-    game INTEGER,
-    question INTEGER,
-    player INTEGER,
-    option TEXT COLLATE pg_catalog."default",
+    game INTEGER NOT NULL,
+    question INTEGER NOT NULL,
+    player INTEGER NOT NULL,
+    option TEXT,
     correct BOOLEAN,
     deleted BOOLEAN DEFAULT false,
-    CONSTRAINT games_questions_pkey PRIMARY KEY (id),
-    CONSTRAINT games_questions_game_fkey FOREIGN KEY (game)
+    CONSTRAINT answers_pkey PRIMARY KEY (id),
+    CONSTRAINT answers_game_fkey FOREIGN KEY (game)
         REFERENCES games (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
-    CONSTRAINT games_questions_player_fkey FOREIGN KEY (player)
+    CONSTRAINT answers_player_fkey FOREIGN KEY (player)
         REFERENCES users (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
-    CONSTRAINT games_questions_question_fkey FOREIGN KEY (question)
+    CONSTRAINT answers_question_fkey FOREIGN KEY (question)
         REFERENCES questions (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
-)
+);
 
 CREATE TABLE tokens
 (
     id SERIAL,
-    token TEXT COLLATE pg_catalog."default",
-    createdat TIMESTAMP WITHOUT TIME ZONE,
-    expires TIMESTAMP WITHOUT TIME ZONE,
-    type CHARACTER(1) COLLATE pg_catalog."default",
-    status CHARACTER(1) COLLATE pg_catalog."default",
-    userid INTEGER,
+    token TEXT NOT NULL,
+    createdat TIMESTAMP NOT NULL,
+    expires TIMESTAMP NOT NULL,
+    type CHARACTER(1) NOT NULL,
+    status CHARACTER(1) NOT NULL,
+    userid INTEGER NOT NULL,
     CONSTRAINT tokens_pkey PRIMARY KEY (id),
     CONSTRAINT tokens_userid_fkey FOREIGN KEY (userid)
         REFERENCES users (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
-)
+);
