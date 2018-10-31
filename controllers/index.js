7884da30037b820fa1df4c5762e2393d8c6be965
ricmtz <ricmtz15@gmail.com>
2018-10-31 17:41:57 -0600

    #104 Update of GamesQuestionsCtrl references to answersCtrl

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,6 @@ exports.usersCtrl = require('./users');
 exports.categoriesCtrl = require('./categories');
 exports.friendsCtrl = require('./friends');
 exports.gamesCtrl = require('./games');
-exports.gamesQuestionsCtrl = require('./games_questions');
+exports.AnswersCtrl = require('./answers');
 exports.questionsCtrl = require('./questions');
 exports.emailsCtrl = require('./emails');
