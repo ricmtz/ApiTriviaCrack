453b2e40f4f44713e279b95c79c85f6b369b2c30
ricmtz <ricmtz15@gmail.com>
2018-11-06 11:55:46 -0600

    #108 friedns, emails and answers orm added to index

--- orm/index.js
+++ orm/index.js
@@ -1,5 +1,8 @@
 exports.CategoriesORM = require('./categories');
 exports.QuestionsORM = require('./questions');
 exports.UsersORM = require('./users');
+exports.FriendsORM = require('./friends');
+exports.EmailsORM = require('./emails');
 exports.GamesORM = require('./games');
+exports.AnswersORM = require('./answers');
 exports.TokensORM = require('./tokens');
