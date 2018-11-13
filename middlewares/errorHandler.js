626ab9b37de8cb64237baadd99bc50c6dfb5fabe
Adriana Castañeda <castalozano.adrigpe@gmail.com>
2018-11-12 22:37:48 -0600

    #114 add removed file if exists error

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,4 +1,11 @@
+const fs = require('fs');
+
 const errorHandler = (err, req, res, next) => {
+    if (req.file !== undefined) {
+        fs.unlink(req.file.path, (error) => {
+            if (error) throw error;
+        });
+    }
     if (err) {
         res.status(err.code || 409).send({ data: err.message });
     } else {
