033f1858b2f841ffac4e564aea9ca903ef60822d
Adriana Castañeda <castalozano.adrigpe@gmail.com>
2018-11-13 14:04:54 -0600

    #144 update methods create y update in users for uploads files

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const multer = require('multer');
 const emailsRouter = require('./emails');
 const friendsRouter = require('./friends');
 const { usersCtrl } = require('../controllers');
-const { rules, auth } = require('../middlewares');
+const { rules, auth, file } = require('../middlewares');
 
 const router = express.Router();
+const upload = multer({ dest: 'temp/' });
 
 router.use(auth.session);
 
@@ -18,13 +20,19 @@ router.get('/', [rules.getAllElements, auth.havePermissions], usersCtrl.getAll);
 router.get('/:nickname', auth.havePermissions, usersCtrl.get);
 
 // Create users.
-router.post('/', [rules.createUser, auth.havePermissions], usersCtrl.create);
+router.post('/',
+    [upload.single('avatar'), rules.createUser,
+        auth.havePermissions, file.changeFolder],
+    usersCtrl.create);
 
 // Delete users.
 router.delete('/:nickname', auth.havePermissions, usersCtrl.delete);
 
 // Update users.
-router.patch('/:nickname', [rules.updateUser, auth.havePermissions], usersCtrl.update);
+router.patch('/:nickname',
+    [upload.single('avatar'), auth.havePermissions,
+        rules.updateUser, file.changeFolder],
+    usersCtrl.update);
 
 router.use('/:nickname/emails', emailsRouter);
 router.use('/:nickname/friends', friendsRouter);
